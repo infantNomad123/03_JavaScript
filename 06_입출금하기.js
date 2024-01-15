99f92fe910027f6d377c1530a766be1d6c83a09b
infantNomad123 <53218837+infantNomad123@users.noreply.github.com>
2024-01-15 22:30:00 +0900

    Add files via upload

---
+++ 06_입출금하기.js
@@ -0,0 +1,58 @@
+/* prompt 사용 연습 */
+
+function test(){
+    const password = prompt("비밀번호를 입력하세요");
+
+    // 확인 -> 입력한 문자열
+    // 취소 -> null
+
+    if(password == null){
+        alert("취소");
+        
+    }else{ //확인
+        
+        //입력한 비밀 번호가 '1234' 가 맞는지 확인
+
+        if(password == '1234'){
+            alert("비밀번호가 일치");
+        }else {
+            alert("비밀번호가 일치하지 않습니다");
+        }
+    }
+}
+
+let balance = 10000;
+const balanceAmnt = document.getElementById("balance");
+const depositAmnt = document.getElementById("amount1");
+balanceAmnt.innerText = `잔액: ${balance} 원`;
+
+
+
+
+function deposit(){
+    const checkDep = Number(depositAmnt.value);
+
+    balance+= checkDep;
+    balanceAmnt.innerText = `잔액: ${balance} 원`;
+    
+}
+function withdrawal(){
+    const checkDep = Number(depositAmnt.value);
+    
+
+    const pw = prompt("비밀번호를 입력해주세요");
+    if(pw != "1234"){
+        alert("비밀번호가 일치하지 않습니다");
+    }else{
+        if(checkDep > balance){
+            alert("출금 금액이 잔액보다 클 수 없습니다");
+        }
+        else{
+            balance-= checkDep;
+            balanceAmnt.innerText = `잔액: ${balance} 원`;
+            alert(`${checkDep}원이 출금 되었습니다. 남은 잔액 : ${balance}원`);
+
+        }
+        
+    }
+}
---
+++ calculator.js
@@ -0,0 +1,95 @@
+const numDisplay = document.getElementById("numPrint");
+let count = 0;
+
+function butt(button){
+
+    switch(button){
+        case '1' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='1';
+            }
+            break;
+        
+        case '2' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='2';
+            }
+            break;
+
+        case '3' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='3';
+            }
+            break;
+
+        case '4' :
+                count++;
+                if(count > 10){
+                    alert(" your time exceeded...piss ");
+                }else{ 
+                    numDisplay.innerText +='4';
+                }
+                break;
+        case '5' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='5';
+            }
+            break;
+        case '6' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='6';
+            }
+            break;
+        case '7' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='7';
+            }
+            break;
+        case '8' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='8';
+            }
+            break;
+        case '9' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='6';
+            }
+            break;
+        case '10' :
+            count++;
+            if(count > 10){
+                alert(" your time exceeded...piss ");
+            }else{ 
+                numDisplay.innerText +='6';
+            }
+            break;
+            
+        
+    }
+    
+
+}
