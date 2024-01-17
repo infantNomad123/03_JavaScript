5755cde0f69a5b2f80f1cd2c23228b18f1aaed18
jkh <jkhwang58@gmail.com>
2024-01-17 17:31:04 +0900

    no message

--- js/08_구구단표.js
+++ js/08_구구단표.js
@@ -5,27 +5,28 @@ function createTable(){
     const th = document.getElementById("thead-row");
 
     const tb = document.getElementById("tbody");
-    
+    let sum = 0;
     th.innerHTML = "";
     tb.innerHTML = "";
 
     //start 부터 end 까지 1씩 증가
-    for(let row =1; row <= 9 ; row++){
+    for(let row =1; row <= 10 ; row++){
         /* th.innerHTML += `<th>${row}단</th>`;
         tb.innerHTML += "<tr>"; */
         let str = "<tr>";
         
          for(let dan = start; dan <= end; dan++){
-            str += `<td>${row}x${dan} = ${row * dan} </td>`;   
+            str += `<td>${row} x${dan} = ${row * dan} </td>`;
+            sum =sum +  dan;   
          }
          str += "</tr>";
-         th.innerHTML = str;
+         tb.innerHTML += str;
          console.log(str);
          
         /* tb.innerHTML += "</tr>"; */
-        
-        
-      
+        th.innerHTML += `<th>${sum}단</th>`;
     }
+
+    
     
 }
